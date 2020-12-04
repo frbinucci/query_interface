13d77652348b9dbb04706ad21348511b9f427bee
bigdata <bigdata@bigdata-VirtualBox>
2020-12-05 00:00:33 +0100

    Implementazione query per manufacturer, displacement e vehicle_category.

--- query_interface/src/main/java/bigdataman/binucci_rinalduzzi/query_interface/QueryInterface.java
+++ query_interface/src/main/java/bigdataman/binucci_rinalduzzi/query_interface/QueryInterface.java
@@ -1,6 +1,7 @@
 package bigdataman.binucci_rinalduzzi.query_interface;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -30,11 +31,15 @@ public class QueryInterface {
     	MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
     	
     	 MongoDatabase database = mongoClient.getDatabase("test");
-    	 MongoCollection<Document> collection = database.getCollection("years");
+    	 MongoCollection<Document> years = database.getCollection("years");
+    	 MongoCollection<Document> manufacturers = database.getCollection("manufacturers");
+    	 MongoCollection<Document> displacement = database.getCollection("displacement");
+    	 MongoCollection<Document> vehicle_category = database.getCollection("vehicle_category");
     	 
     	 int choiche=1;
     	 while(choiche!=0)
     	 {
+    		 System.out.println();
 	    	 System.out.println("Which collection do you want to query?");
 	    	 System.out.println("1 - for years");
 	    	 System.out.println("2 - for manufacturers");
@@ -45,13 +50,14 @@ public class QueryInterface {
 
 	    	 choiche = KeyboardInput.inputInteger();
 	    	 int yearChoiche;
+	    	 int manChoiche;
 	    	 
 	    	 switch(choiche)
 	    	 {
 		    	 case 1:
 			    		 System.out.print("Insert a year: ");
 			    		 String year = KeyboardInput.inputString();
-		    			 Document yearDoc = collection.find(eq("_id", year)).first();
+		    			 Document yearDoc = years.find(eq("_id", year)).first();
 		    			 if(yearDoc!=null)
 		    			 {
 				    		 do
@@ -93,7 +99,7 @@ public class QueryInterface {
 					    		 		break;
 					    		 	case 3:
 					    		 		String meanEmissions = yearDoc.get("mean_emissions").toString();
-					    		 		System.out.println("Mean emissions for "+year+ "are: "+meanEmissions);
+					    		 		System.out.println("Mean emissions for "+year+ " are: "+meanEmissions);
 					    		 		System.out.println();
 					    		 		break;
 					    		 	default:
@@ -107,8 +113,87 @@ public class QueryInterface {
 		    				 System.out.println("There are no data associated to "+year);
 		    			 }
 			    		break;
-			    	default:
-			    		break;
+		    	default:
+		    		break;
+			    case 2:
+			    	System.out.print("Insert a manufacturer (e.g. \"Audi\"): ");
+		    		String manufacturer = KeyboardInput.inputString();
+	    			Document manDoc = manufacturers.find(eq("_id", manufacturer)).first();
+	    			if(manDoc!=null) {
+	    				do {
+	    					System.out.println("Which information do you want to know about "+manufacturer);
+				    		System.out.println("1 - Min consumption model with consumption");
+				    		System.out.println("2 - Max emissions by year");
+				    		System.out.println("3 - Mean emissions");
+				    		System.out.println("0 - Go back");
+				    		manChoiche = KeyboardInput.inputInteger();
+				    		switch(manChoiche) {
+					    		case 1:
+				    		 		String model = manDoc.get("min_cons_model").toString();
+				    		 		String min_year = manDoc.get("min_cons_year").toString();
+				    		 		String min_cons = manDoc.get("min_cons").toString();
+				    		 		System.out.println("The model is: "+model+", from year "+min_year);
+				    		 		System.out.println("Min consumption is: "+min_cons);
+				    		 		System.out.println();
+				    		 		break;
+				    		 	case 2:
+				    		 		List<Document> obj = (List) manDoc.get("max_emissions_by_year");
+				    		 		//System.out.println(obj.size());
+				    		 		for(Document doc : obj){
+				    		 			String y = doc.getString("year");
+				    		 			String m = doc.getString("model");
+				    		 			String e = doc.getString("emission");
+				    		 			System.out.println("YEAR: "+y+", MODEL: "+m+", EMISSION: "+e);				    		 			
+				    		 		}
+				    		 		System.out.println();			
+				    		 		break;
+				    		 	case 3:
+				    		 		String meanEmissions = manDoc.get("mean_emissions").toString();
+				    		 		System.out.println("Mean emissions for "+manufacturer+ " are: "+meanEmissions);
+				    		 		System.out.println();
+				    		 		break;
+				    		 	default:
+				    		 		break;
+				    		}
+	    				}while(manChoiche!=0);
+
+	    				
+	    			}else{
+	    				System.out.println("There are no data associated to "+manufacturer);
+	    			}
+	    			break;
+			    case 3:
+			    	int exit=-1;
+			    	do {
+				    	System.out.print("Insert a displacement (e.g. \"2.0\") and I will show you the mean consumption"
+				    			+ " of an engine of that displacement (insert 0 to exit): ");
+				    	String disp = KeyboardInput.inputString();
+		    			Document dispDoc = displacement.find(eq("_id", disp)).first();
+		    			if(dispDoc!=null) {
+		    				String mean_cons = dispDoc.get("mean_consumption").toString();
+		    		 		System.out.println("Mean consumption is: "+mean_cons);
+		    		 		System.out.println();
+		    			}else {
+		    				System.out.println("There are no data associated to "+disp);
+		    			}
+			    	}while(exit!=0);
+			    	break;
+			    case 4:
+			    	int ex=-1;
+			    	do {
+				    	System.out.print("Insert a vehicle category (e.g. \"Midsize Cars\") and I will show you the mean consumption"
+				    			+ " of that category (insert 0 to exit): ");
+				    	String cat = KeyboardInput.inputString();
+		    			Document catDoc = vehicle_category.find(eq("_id", cat)).first();
+		    			if(catDoc!=null) {
+		    				String mean_cons = catDoc.get("mean_consumption").toString();
+		    		 		System.out.println("Mean consumption is: "+mean_cons);
+		    		 		System.out.println();
+		    			}else {
+		    				System.out.println("There are no data associated to "+cat);
+		    			}
+			    	}while(ex!=0);
+			    	break;
 			    case 5:
 			    	System.out.println("Which kind of timeseries do you want to generate?");
 			    	System.out.println("1 - for max consumption");
@@ -121,12 +206,12 @@ public class QueryInterface {
 			    	FindIterable<Document> timeSeriesDoc;
 			    	if(timeSeriesChoiche==1)
 			    	{
-			    		timeSeriesDoc = collection.find(and(gte("_id", yearStart), lte("_id", yearStop))).projection(Projections.include("_id", "max_cons"));
+			    		timeSeriesDoc = years.find(and(gte("_id", yearStart), lte("_id", yearStop))).projection(Projections.include("_id", "max_cons"));
 			    		WriteJSONToFIle.writeToFile("max_emissions"+yearStart+"_"+yearStop+".js",timeSeriesDoc);
 			    	}
 			    	else
 			    	{
-			    		timeSeriesDoc = collection.find(and(gte("_id", yearStart), lte("_id", yearStop))).projection(Projections.include("_id", "mean_emissions"));
+			    		timeSeriesDoc = years.find(and(gte("_id", yearStart), lte("_id", yearStop))).projection(Projections.include("_id", "mean_emissions"));
 			    		WriteJSONToFIle.writeToFile("mean_emissions"+yearStart+"_"+yearStop+".js",timeSeriesDoc);
 			    	}
 			    		
